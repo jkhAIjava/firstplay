3b5bfc8978756ccaf8d92c35d276be001384da5e
jikuanhao <352531148@qq.com>
2018-01-04 14:26:43 +0800

    测试

---
+++ mybatis01/src/com/controller/BaseDictController.java
@@ -0,0 +1,38 @@
+package com.jit.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jit.common.utils.Page;
+import com.jit.model.BaseDict;
+import com.jit.model.Customer;
+import com.jit.model.QueryVo;
+import com.jit.service.BusinessService;
+//¹þ¹þ¹þ
+@Controller
+public class BaseDictController {
+	
+	@Resource
+	private BusinessService bs;
+	
+	@RequestMapping("/list.action")
+	public String test1(Model model){
+		List<BaseDict> industryType = bs.getBaseDictBySource("001");
+		List<BaseDict> fromType = bs.getBaseDictBySource("002");
+		List<BaseDict> levelType = bs.getBaseDictBySource("006");
+		model.addAttribute("industryType", industryType);
+		model.addAttribute("fromType", fromType);
+		model.addAttribute("levelType", levelType);
+		
+//		if(page!=null){
+//			model.addAttribute("pag", arg1)
+//		}
+		
+		return "/WEB-INF/jsp/customer.jsp";
+	}
+}
---
+++ mybatis01/src/com/controller/CustomerController.java
@@ -0,0 +1,57 @@
+package com.jit.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jit.common.utils.Page;
+import com.jit.model.BaseDict;
+import com.jit.model.Customer;
+import com.jit.model.QueryVo;
+import com.jit.service.BusinessService;
+//github真是个好东西哟
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Resource
+	private BusinessService bs;
+	
+	@RequestMapping("/query.action")
+	public String query(Model model,QueryVo vo){
+		Page<Customer> page= bs.getPage(vo);
+		model.addAttribute("vo", vo);
+		model.addAttribute("page", page);
+		return "/list.action";
+	}
+	
+	@RequestMapping("/toEdit.action")
+	public @ResponseBody Customer toEdit(Model model,int id){
+		
+		Customer customer = bs.selectCustomerById(id);
+		
+		return customer;
+	}
+	
+	@RequestMapping("/edit.action")
+	public @ResponseBody void edit(Model model,Customer customer){
+		Logger logger = Logger.getLogger(CustomerController.class);
+		logger.info("ID为: "+customer.getCust_id()+" 的customer被修改");
+		bs.editCustomer(customer);
+	}
+	
+	@RequestMapping("/delete.action")
+	public @ResponseBody void delete(Model model,int id){
+		bs.deleteCustomerById(id);
+	}
+	
+	
+}
